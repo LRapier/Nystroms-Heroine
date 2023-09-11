a4deb01d7d454a4a5ba255d1c29d33354b5862cb
LRapier <98057284+LRapier@users.noreply.github.com>
2023-09-10 22:41:30 -0700

    Heroine V2

--- Assets/Heroine.cs
+++ Assets/Heroine.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class Heroine : MonoBehaviour
 {
-    private HeroineState _state = new StandingState();
+    private HeroineState standState = new StandingState();
     public ParticleSystem ps;
     public Animator anim;
 
     private void Start()
     {
-        _state.Enter(this);
+        standState.Enter(this);
         anim.SetBool("Backflip", false);
     }
 
     private void Update()
     {
-        HeroineState state = _state.HandleInput(this);
+        HeroineState state = standState.HandleInput(this);
         if (state != null)
         {
-            _state.Exit(this);
-            _state = state;
-            _state.Enter(this);
+            standState.Exit(this);
+            standState = state;
+            standState.Enter(this);
         }
-        _state.Update(this);
+        standState.Update(this);
     }
 }
